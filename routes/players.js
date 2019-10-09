1adc174ac845f20cb6fe1ef146ad9f1426a562f3
Diego Mayen <dmayen@digitalgeko.com>
2019-10-09 16:19:22 -0600

    delete and put messages deleted

--- routes/players.js
+++ routes/players.js
@@ -8,7 +8,9 @@ const baseUrl = route + version + theme;
 
 const players = require('../routes/players.json');
 
-router.get(baseUrl, (req, res) => {
+var requestedPlayer;
+
+/*router.get(baseUrl, (req, res) => {
     const { id } = req.body;
     if (!id) {
         // return all players and code 200
@@ -22,16 +24,35 @@ router.get(baseUrl, (req, res) => {
         // return error 404
         res.status(404).send('Not found');
     }
+})*/
+
+router.get(baseUrl, (req, res) => {
+    // return all players and code 200
+    console.log('all players request');
+    res.status(200).json(players);
+})
+
+router.get(baseUrl  + '/:id', (req, res) => {
+    const { id } = req.params;
+    console.log('player with id: ' + id + ' request');
+    if(playerExist(id)){
+        // return specific player and code 200
+        res.status(200).json(getPlayer(id));
+    }else{
+        // return error 404
+        res.status(404).send('Not found');
+    }
 })
 
 router.post(baseUrl, (req, res) => {
     const { name, team, age, position, country } = req.body;
+    console.log(name + team + age + position + country);
     if(name && team && age && position && country){
         //add player
         var id = generateId();
         const newPlayer = {id, ...req.body};
         players.push(newPlayer);
-        res.status(201).send('Player added');
+        res.status(201).send('Player with id: ' + id + ' added');
     }else{
         res.status(400).send('Can\'t post');
     }
@@ -40,8 +61,7 @@ router.post(baseUrl, (req, res) => {
 router.put(baseUrl + '/:id', (req, res) => {
     const { id } = req.params;
     const { name, team, age, position, country} = req.body;
-
-    _.each(players, (player, i) => {
+    for(var player of players){
         if(player.id == id){
             player.name = name;
             player.team = team;
@@ -50,35 +70,60 @@ router.put(baseUrl + '/:id', (req, res) => {
             player.country = country;
             console.log(players);
             res.status(204).send('Player with id: ' + id + ' was updated');
+            return;
         }
-    })
+    }
     res.status(404).send('There was no player with the id: ' + id);
 })
 
 router.delete(baseUrl + '/:id', (req, res) => {
     const { id } = req.params;
-    _.each(players, (player, i) => {
+    var i = 0;
+    for(var player of players){
         if(player.id == id){
             players.splice(i, 1);
             console.log(players);
             res.status(204).send('Player with id: ' + id + ' was deleted');
+            return;
         }
-    })
+        i++;
+    }
     res.status(404).send('There was no player with the id: ' + id);
 })
 
 function playerExist(id){
-    return true;
+    for(var player of players){
+        if(player.id == id){
+            requestedPlayer = player;
+            return true;
+        }
+    }
+    return false;
 }
 
 function getPlayer(id){
     //change function to return just one player
-    return players;
+    if(id = requestedPlayer.id){
+        return requestedPlayer;
+    }else{
+        for(var player of players){
+            if(player.id == id){
+                requestedPlayer = player;
+                return requestedPlayer;
+            }
+        }
+    }
 }
 
 function generateId(){
-    //change function to return an id
-    return 1;
+    let highestId = 0;
+    for(var player of players){
+        if(player.id > highestId){
+            highestId = player.id;
+        }
+    }
+    highestId++;
+    return highestId;
 }
 
 module.exports = router;
